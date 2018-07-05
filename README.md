ceb3167c1a843812cc70a60bb1c11761ce3df9bc
Misha Milovidov <m.milovidov@icloud.com>
2018-07-05 15:46:30 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Modern React with Redux
+
+This is the accompanying project that goes along with Stephen Grider's course, "[Modern React with Redux](https://www.udemy.com/react-redux/)." This React app is a video search and player application that uses the YouTube API.
+
+### Getting Started
+
+```
+> clone
+> cd modern-react-with-redux
+> npm install
+> npm start
+```
---
+++ src/index.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/SearchBar';
+import VideoList from './components/VideoList';
+import VideoDetail from './components/VideoDetail';
+const API_KEY = 'AIzaSyBzQs6otzIa3r2H6s68F6NLR0ctu9l-Sx4';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
+
+    this.videoSearch('surfboards');
+  }
+
+  videoSearch(term) {
+    YTSearch({key: API_KEY, term: term}, (videos) => {
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar onSearchTermChange={term => this.videoSearch(term)} />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+          onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+          videos={this.state.videos} />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector('.container'));
